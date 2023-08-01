575e178585101375484d553843ec541f67e57d40
Jozef Nagy <schkwve@gmail.com>
2023-08-01 23:08:20 +0200

    Added fallback for terminal_get_window_size()

--- src/terminal.c
+++ src/terminal.c
@@ -38,6 +38,8 @@ int terminal_get_window_size(int *rows, int *cols)
 	struct winsize ws;
 
 	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B]]", 12) != 12) return -1;
+		input_get_keypress();
 		return -1;
 	} else {
 		*cols = ws.ws_col;
